14d52589f28d146595dcabf5c0953f9db35a4faa
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-10-25 10:50:59 +0100

    Implemented search parameters on SearchPage

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,3 +1,42 @@
+/**@jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Page } from '../components/Page';
+import { QuestionList } from '../components/QuestionList';
+import { QuestionData, searchQuestions } from '../QuestionsData';
 
-export const SearchPage = () => <Page title="Search Results">{null}</Page>;
+export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+  const [questions, setQuestions] = useState<QuestionData[]>([]);
+
+  //Get criteria query
+  const search = searchParams.get('criteria') || '';
+
+  useEffect(() => {
+    const doSearch = async (criteria: string) => {
+      const foundResults = await searchQuestions(criteria);
+
+      setQuestions(foundResults);
+    };
+
+    doSearch(search);
+  }, [search]);
+
+  return (
+    <Page title="Search Results">
+      {search && (
+        <p
+          css={css`
+            font-size: 16px;
+            font-style: italic;
+            margin-top: 0px;
+          `}
+        >
+          for "{search}"
+        </p>
+      )}
+      <QuestionList data={questions} />
+    </Page>
+  );
+};
